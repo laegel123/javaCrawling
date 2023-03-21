6cbd20e01965bb245d7cff1a9ea51b1e8248c607
laegel <laegel@selper.co.kr>
2023-03-21 13:39:55 +0900

    Title : 네이버 쇼핑 크롤링 로직

--- src/main/java/crawling/test/controller/CrawlingController.java
+++ src/main/java/crawling/test/controller/CrawlingController.java
@@ -2,11 +2,10 @@ package crawling.test.controller;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,34 +19,47 @@ public class CrawlingController {
 
     @GetMapping("/crawling/{param}")
     public Result crawlingTest(@PathVariable("param") String param) {
-        String baseUrl = "https://search.shopping.naver.com/search/all?&sort=price_asc&query=";
-        //String param = "에브리타임";
-
+        String baseUrl = "https://search.shopping.naver.com/search/all?&fo=true&sort=price_asc&query=";
         String requestUrl = baseUrl + param;
 
         Map<String, String> resultMap = null;
         try {
+            Connection.Response response = Jsoup.connect(requestUrl)
+                    .method(Connection.Method.GET)
+                    .timeout(1000)
+                    .execute();
+            Document doc = response.parse();
 
-            Document doc = null;
-            doc = Jsoup.connect(requestUrl).get();
+            Element firstNameElement = doc.select("div.basicList_title__VfX3c").first();
+            Element firstPriceElement = doc.select("span.price_num__S2p_v").first();
+            Element firstShopElement = doc.select("a.basicList_mall__BC5Xu").first();
 
-            Elements nameElements = doc.getElementsByAttributeValue("class", "basicList_title__VfX3c");
-            Element firstNameElement = nameElements.get(0);
+            String productName = "";
+            String productLink = "";
+            String productPrice = "";
+            String productShop = "";
+            if (firstNameElement != null) {
+                productName = firstNameElement.select("a").text();
+                productLink = firstNameElement.select("a").attr("href");
+            }
+            if (firstPriceElement != null) {
+                productPrice = firstPriceElement.text();
+            }
 
-            Elements priceElements = doc.getElementsByAttributeValue("class", "price_num__S2p_v");
-            Element firstPriceElement = priceElements.get(0);
-
-            String productName = firstNameElement.select("a").text();
-            String productPrice = firstPriceElement.text();
+            if (firstShopElement != null) {
+                productShop = firstShopElement.text().equals("")  ? firstShopElement.select("img").attr("alt") : firstShopElement.text();
+            }
 
             resultMap = new HashMap<>();
             resultMap.put("productName", productName);
             resultMap.put("productPrice", productPrice);
+            resultMap.put("productLink", productLink);
+            resultMap.put("productShop", productShop);
+
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
 
         return new Result(resultMap);
     }
